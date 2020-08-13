c6253d73c50bd3087767f1f212630f5058f0156c
itsr01 <rauldiasmendes94@gmail.com>
2020-08-13 11:33:52 -0400

    200813-06

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,18 +1,9 @@
-// This is the service worker with the Cache-first network
-
-const CACHE = "pwabuilder-precache";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.1/workbox-sw.js');
 
 self.addEventListener("message", (event) => {
-    if (event.data && event.data.type === "SKIP_WAITING") {
-        self.skipWaiting();
-    }
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-workbox.routing.registerRoute(
-    new RegExp('/*'),
-    new workbox.strategies.CacheFirst({
-        cacheName: CACHE
-    })
-);
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
